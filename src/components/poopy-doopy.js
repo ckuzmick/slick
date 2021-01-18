31b854590fcc74527ac582f02f13c857cc6db444
ck <ck@cks-macbook-pro.lan>
2021-01-18 14:29:49 -0500

    new components added

---
+++ src/components/poopy-doopy.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactCompareImage from 'react-compare-image';
+
+
+const PoopyDoopy = () => {
+    return (
+        <div>
+            <h1>PoopyDoopy</h1>
+            <p>sha la la loopy.</p>
+            <ReactCompareImage leftImage="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/14112506/Pembroke-Welsh-Corgi-standing-outdoors-in-the-fall.jpg" rightImage="https://avatars2.githubusercontent.com/u/33187769?s=460&u=651d264a83c05be244d65f5fe67922c38e389475&v=4" />;
+        </div>
+    )
+}
+
+export default PoopyDoopy
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,13 +2,11 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import PoopyDoopy from "../components/poopy-doopy"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <PoopyDoopy />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
